17abf88ee39046b03c154706b242adcb9ace5fda
afisl1108-bit <agisl1108@gmail.com>
2026-04-15 18:10:09 +0900

    슬라이드 막대기 문제

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,21 @@ new Swiper('.category_swiper', {
     slidesPerView: 4,
     spaceBetween: 0,
 });
+
+// Curated Essentials 카테고리 변경
+const tabs = document.querySelectorAll('.category_tab li');
+const swipers = document.querySelectorAll('.curated_swiper');
+
+function setActiveTab(target) {
+    tabs.forEach(t => t.classList.remove('active'));
+    swipers.forEach(s => s.classList.remove('active'));
+    document.querySelector(`[data-tab="${target}"]`).classList.add('active');
+    document.querySelector(`.curated_swiper.${target}`).classList.add('active');
+    }
+    setActiveTab('body');
+    tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        setActiveTab(tab.dataset.tab);
+    });
+});
+
